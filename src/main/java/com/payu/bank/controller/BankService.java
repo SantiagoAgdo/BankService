211405b86b28458a7382cde249aeebb278a03b2b
santiagoagudelo <agudelo.santigo@gmail.com>
2024-07-05 16:09:53 -0500

    Initial commit

---
+++ src/main/java/com/payu/bank/controller/BankService.java
@@ -0,0 +1,17 @@
+package com.payu.bank.controller;
+
+import com.payu.bank.dto.CreditCardTransaction;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bank")
+public class BankService {
+    @PostMapping("/process")
+    public String process(@RequestBody CreditCardTransaction transaction) {
+        // Simple bank processing logic
+        return "SUCCESS";
+    }
+}
---
+++ src/main/java/com/payu/bank/dto/CreditCardTransaction.java
@@ -0,0 +1,17 @@
+package com.payu.bank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditCardTransaction {
+    private Long id;
+    private String cardNumber;
+    private Double amount;
+    private String status;
+    private String transactionType;
+
+}
